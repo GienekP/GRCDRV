39278925c73cf921d19a4a681fb9e49ab33b0957
GienekP <97046310+GienekP@users.noreply.github.com>
2022-02-22 21:10:58 +0100

    Add files via upload

---
+++ go.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#./mads grcdrv_sys.asm -o:grcdrv.sys
+./mads grcdrv_reloc.asm -o:grcdrv.xex
+./mads grcdrv_cas.asm -o:grcdrv_cas.boot
+./xex2cas grcdrv_cas.boot grcdrv.cas /b
+rm grcdrv_cas.boot
