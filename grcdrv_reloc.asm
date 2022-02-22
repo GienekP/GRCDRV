af1595e617ce9707981065eb3d09d115c4b3949c
GienekP <97046310+GienekP@users.noreply.github.com>
2022-02-22 21:18:06 +0100

    Update grcdrv_reloc.asm

--- grcdrv_reloc.asm
+++ grcdrv_reloc.asm
@@ -7,7 +7,7 @@
 .LOCAL	
 ;-----------------------------------------------------------------------
 DEST    = $00			; use address LNFLG & NGFLAG as a temp
-PTR		= $1D
+PTR	= $1D
 ;-----------------------------------------------------------------------		
 CASINI  = $02
 BOOT?   = $09
@@ -23,8 +23,8 @@ CASBUF  = $0400
 		ORG $4000
 		RUN START
 		
-		pla				; for BASIC
-START	lda DEST+1		; store LNFLG & NGFLAG
+		pla			; for BASIC
+START		lda DEST+1		; store LNFLG & NGFLAG
 		pha
 		lda DEST
 		pha
@@ -55,7 +55,7 @@ START	lda DEST+1		; store LNFLG & NGFLAG
 		lda #$60
 		sta CASBUF+12
 		jsr CASBUF		; PC address to stack
-MARK	clc				; <-this address-1 is detected
+MARK		clc			; <-this address-1 is detected
 		lda DEST
 		adc #<(GNEWDEV+1-MARK)
 		sta PTR
@@ -69,7 +69,7 @@ MARK	clc				; <-this address-1 is detected
 		lda MEMLO+1
 		sta DEST+1
 		
-		clc				; incrase MEMLO
+		clc			; incrase MEMLO
 		lda DEST
 		adc #<(GENDPRO+1-GNEWDEV)
 		sta MEMLO
@@ -90,7 +90,7 @@ MARK	clc				; <-this address-1 is detected
 		pha	
 		
 		ldx #0
-CPYLOOP	lda (PTR,X)
+CPYLOOP		lda (PTR,X)
 		sta (DEST,X)
 	
 		clc
@@ -129,7 +129,7 @@ L1		lda MEMLO
 		sta PTR+1
 		
 		; Recalculate VECTOR TABLE
-		clc				; find GNEWDEV address
+		clc			; find GNEWDEV address
 		lda DEST
 		adc #<(GDRIVER-GNEWDEV)
 		sta DEST
@@ -237,7 +237,7 @@ L1		lda MEMLO
 		cmp #$00
 		bne END
 		
-VCAS	lda #$02		; absence DOS
+VCAS		lda #$02		; absence DOS
 		sta BOOT?
 
 		lda DEST
@@ -249,11 +249,11 @@ VCAS	lda #$02		; absence DOS
 		sta DOSINI+1
 		;sta DOSVEC+1
 		
-END		pla				; restore $1D
+END		pla			; restore $1D
 		sta PTR
 		pla
 		sta PTR+1
-		pla				; restore LNFLG & NGFLAG
+		pla			; restore LNFLG & NGFLAG
 		sta DEST
 		pla
 		sta DEST+1
